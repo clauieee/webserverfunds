c6a3ee54b4b97e4e00dc608ba5afbe0156ace097
clauieee <clauie_elegado@yahoo.com>
2025-09-24 13:00:48 -0400

    libraryResponse - changes

--- libraryResponse.php
+++ libraryResponse.php
@@ -11,9 +11,9 @@
             $author = htmlspecialchars($_POST['author']);
             $genre  = htmlspecialchars($_POST['genre']);
             echo "Hello reader!<br>";
-            echo "The book you are looking for is: $title<br>";
+            echo "The book you are recommending is: $title<br>";
             echo "The author of the book is: $author<br>";
-            echo "Your favorite genre is: $genre<br>";
+            echo "Your chosen genre is: $genre<br>";
         ?>
     </body>
 </html>
