38221261f6b7aaf56171368efafe9d05ad993416
clauieee <clauie_elegado@yahoo.com>
2025-09-24 12:58:33 -0400

    libraryResponse - post display

--- libraryResponse.php
+++ libraryResponse.php
@@ -5,11 +5,15 @@
     </head>
     <body>
         <h2>Library Form Results</h2>
-        <p>GET:
-            <?php var_dump($_GET); ?>
-        </p>
-        <p>POST:
-            <?php var_dump($_POST); ?>
-        </p>
+        <?php
+            // Sanitize inputs from POST
+            $title  = htmlspecialchars($_POST['title']);
+            $author = htmlspecialchars($_POST['author']);
+            $genre  = htmlspecialchars($_POST['genre']);
+            echo "Hello reader!<br>";
+            echo "The book you are looking for is: $title<br>";
+            echo "The author of the book is: $author<br>";
+            echo "Your favorite genre is: $genre<br>";
+        ?>
     </body>
 </html>
