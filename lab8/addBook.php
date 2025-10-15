dcb9079248d1768eff3ec9e5bba2dc357358e737
clauieee <clauie_elegado@yahoo.com>
2025-10-15 13:11:51 -0400

    changes

--- lab8/addBook.php
+++ lab8/addBook.php
@@ -1,37 +1,47 @@
-<?php
-//Database Connection Settings
-$servername = "localhost";
-$username = "www-data";
-$password = "password";
-$dbname = "libraryDB";
+<!Doctype html>
+<html>
+    <head>
+        <?php
+            //Database Connection Settings
+            $server = "localhost";
+            $username = "www-data";
+            $password = "password";
+            $database = "libraryDB";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+            $conn = mysqli_connect($server, $username, $password, $database);
+            if (!$conn)
+                die("Connection Failed: {mysqli_connect_error()}");
 
+            //Retrieve Form Data
+            $title = $_POST['title'];
+            $author = $_POST['author'];
+            $genre = $_POST['genre'];
 
-//Retrieve Form Data
-$title = $_POST['title'];
-$author = $_POST['author'];
-$genre = $_POST['genre'];
+            //Insert Book Data into Table (books)
+            $sql = "INSERT INTO books (title, author, genre) 
+                    VALUES ('$title', '$author', '$genre')";
+        ?>
+    </head>
 
-//Insert Book Data into Table (books)
-$sql = "INSERT INTO books (title, author, genre) 
-        VALUES ('$title', '$author', '$genre')";
+    <body>
+        <?php
+            if ($conn->query($sql)==TRUE)
+            {
+                echo "<h2>Book Added Successfully!<h2/>";
+                echo "Title: ".htmlspecialchars($title)."<br/>";
+                echo "Author: ".htmlspecialchars($author)."<br/>";
+                echo "Genre: ".htmlspecialchars($genre)."<br/>";
+                
+                echo "<a href='addBook.html'>Add Another Book </a><br/>";
+                echo "<a href='searchBook.html'>Search for a Book </a><br/>";
+            }
+            else
+            {
+                echo "<p style='color:red;'>Error Adding Book:"
+            }
 
-if ($conn->query($sql)==TRUE)
-{
-    echo "<h2>Book Added Successfully!<h2/>";
-    echo "Title: ".htmlspecialchars($title)."<br/>";
-    echo "Author: ".htmlspecialchars($author)."<br/>";
-    echo "Genre: ".htmlspecialchars($genre)."<br/>";
-    
-    echo "<a href='addBook.html'>Add Annother Book </a><br/>";
-    echo "<a href='searchBook.html'>Search for a Book </a><br/>";
-}
-else
-{
-    echo "<p style='color:red;'>Error Adding Book:"
-}
-
-$conn->close();
-?>
+            $conn->close();
+        ?>
+    </body>
+</html>
 
--- lab8/searchBook.php
+++ lab8/searchBook.php
@@ -1,36 +1,45 @@
-<?php
-//Database Connection Settings
-$servername = "localhost";
-$username = "www-data";
-$password = "password";
-$dbname = "libraryDB";
+<!Doctype html>
+<html>
+    <head>
+        <?php
+        //Database Connection Settings
+        $server = "localhost";
+        $username = "www-data";
+        $password = "password";
+        $database = "libraryDB";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = mysqli_connect($server, $username, $password, $database);
+        if (!$conn)
+            die("Connection Failed: {mysqli_connect_error()}");
 
-//Retrieve User Input for Search
-$genre = $_GET['genre'];
-echo "<h2>Search Results for Genre:<i>".htmlspecialchars($genre). "</i></h2>";
+        //Retrieve User Input for Search
+        $genre = $_GET['genre'];
+        echo "<h2>Search Results for Genre:<i>".htmlspecialchars($genre). "</i></h2>";
 
-//Retrieve Matching Books
-$sql = "SELECT title, author FROM books WHERE genre LIKE '%$genre%'";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-   echo "<table border='1' cellpadding='5'>";
-   echo "<tr><th>Title</th><th>Author</th><th>Year Published</th></tr>";
-   while ($row = $result->fetch_assoc()) {
-       echo "<tr>";
-       echo "<td>" . htmlspecialchars($row['title']) . "</td>";
-       echo "<td>" . htmlspecialchars($row['author']) . "</td>";
-       echo "<td>" . htmlspecialchars($row['genre']) . "</td>";
-       echo "</tr>";
-   }
-   echo "</table><br/>";
-   echo "<a href='addBook.html'>Add Another Book</a><br/>";
-   echo "<a href='searchBook.html'>New Search</a>";
-} else {
-   echo "<p style='color:red;'>No Books Found in This Genre.</p>";
-   echo "<a href='searchBook.html'>Try again</a>";
-}
-$conn->close();
-?>
+        //Retrieve Matching Books
+        $sql = "SELECT title, author FROM books WHERE genre LIKE '%$genre%'";
+        $result = mysqli_query($conn, $sql);
+        ?>
+    <body>    
+        <?php
+            if ($result->num_rows > 0) {
+            echo "<table border='1' cellpadding='5'>";
+            echo "<tr><th>Title</th><th>Author</th><th>Year Published</th></tr>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . htmlspecialchars($row['title']) . "</td>";
+                echo "<td>" . htmlspecialchars($row['author']) . "</td>";
+                echo "<td>" . htmlspecialchars($row['genre']) . "</td>";
+                echo "</tr>";
+            }
+            echo "</table><br/>";
+            echo "<a href='addBook.html'>Add Another Book</a><br/>";
+            echo "<a href='searchBook.html'>New Search</a>";
+            } else {
+            echo "<p style='color:red;'>No Books Found in This Genre.</p>";
+            echo "<a href='searchBook.html'>Try again</a>";
+            }
+            $conn->close();
+        ?>
+    </body>
+</html>
