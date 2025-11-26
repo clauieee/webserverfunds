be2829effcbd228081f655b54040484ea0dac5a9
clauieee <clauie_elegado@yahoo.com>
2025-11-26 13:41:56 -0500

    .

--- lab11/sensor.php
+++ lab11/sensor.php
@@ -1,17 +1,18 @@
 <?php
     // Run the BME280 binary
     $output = shell_exec("./bme280");
-    // Split the output by spaces
-    $parts = explode(" ", $output);
+    echo $output;
+    // // Split the output by spaces
+    // $parts = explode(" ", $output);
     
-    $temperature = $parts[0] ?? "N/A";
-    $pressure    = $parts[1] ?? "N/A";
-    $altitude    = $parts[2] ?? "N/A";
+    // $temperature = $parts[0] ?? "N/A";
+    // $pressure    = $parts[1] ?? "N/A";
+    // $altitude    = $parts[2] ?? "N/A";
     
-    // Return JSON
-    echo json_encode([
-    "temperature" => $temperature,
-    "pressure"    => $pressure,
-    "altitude"    => $altitude
-    ]);
+    // // Return JSON
+    // echo json_encode([
+    // "temperature" => $temperature,
+    // "pressure"    => $pressure,
+    // "altitude"    => $altitude
+    // ]);
 ?>
