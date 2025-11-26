0b480718926b176dd2d78f1f02a395ce0e2daeae
clauieee <clauie_elegado@yahoo.com>
2025-11-26 12:29:58 -0500

    .

--- lab11/sensor.php
+++ lab11/sensor.php
@@ -6,25 +6,9 @@
     <body>
         <h2>LED Control Results</h2>
             <?php
-                // Full path to your bme280 binary
-                $binary = "/bme280";
-                // Make sure the binary exists and is executable
-                if (!file_exists($binary)) {
-                echo json_encode(["error" => "Binary not found at $binary"]);
-                exit;
-                }
-                // Run the binary and capture output
-                $output = shell_exec($binary);
-                // Parse the values using regex
-                preg_match("/Temp:\s*([0-9\.\-]+)/", $output, $temp);
-                preg_match("/Pressure:\s*([0-9\.\-]+)/", $output, $press);
-                preg_match("/Altitude:\s*([0-9\.\-]+)/", $output, $alt);
-                // Return JSON response
-                echo json_encode([
-                "temperature" => $temp[1] ?? "N/A",
-                "pressure"    => $press[1] ?? "N/A",
-                "altitude"    => $alt[1] ?? "N/A"
-                ]);
+                $binary = "/raspberry-pi-bme280/bme280";
+                $output = shell_exec($binary . " 2>&1");
+                echo $output;
             ?>
         <br>
     </body>
