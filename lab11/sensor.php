b0d7c0a2f4d0d617bdd498529adfd5d5b34d604d
clauieee <clauie_elegado@yahoo.com>
2025-11-26 13:14:50 -0500

    .

--- lab11/sensor.php
+++ lab11/sensor.php
@@ -1,21 +1,17 @@
 <?php
     header("Content-Type: application/json");
     $binary = "/bme280";
-    $output = shell_exec("./bme280");
+    $output = shell_exec($binary . " 2>&1");
     $temperature = $pressure = $altitude = "N/A";
-
     if (preg_match("/Temp:\s*([0-9\.\-]+)/i", $output, $match)) {
     $temperature = $match[1];
     }
-
     if (preg_match("/Pressure:\s*([0-9\.\-]+)/i", $output, $match)) {
     $pressure = $match[1];
     }
-
     if (preg_match("/Altitude:\s*([0-9\.\-]+)/i", $output, $match)) {
     $altitude = $match[1];
     }
-    
     // Return JSON
     echo json_encode([
     "temperature" => $temperature,
